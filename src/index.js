8122bf0da78a5eea0c5a5e4432352166f0ce0359
Roman <roman.panchenko@dstarlab.com>
2021-03-10 16:52:52 +0200

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arrOfNumbers = n.toString().split('');
+    let reversedArr = [];
+
+    arrOfNumbers.forEach(item => reversedArr.unshift(item));
+    return parseInt(reversedArr.join(''));
+
 }
+
+// console.log(reverse(123))
+
